3eabd180a03a4d0129bb09f431734d1609b8b844
Aerik777 <aerikshah43@gmail.com>
2025-08-11 13:33:25 +0545

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Main/Main';
 import Footer from './components/Footer';
+import Main from './components/Main/Main';
 
 
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <Main/>
+      <Main />
       <Footer/>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import Button from '../common/Button';
+
+
+
 
 const Header = () => {
+  // State to manage the button status
+  const [status, setStatus] = useState(true);
+
+  useEffect(() => {
+    // This effect runs once when the component mounts 
+    console.log("Header component mounted");
+  }, []);
+
+  // Function to toggle the button status
+  const toggleStatus = () => {
+    setStatus(!status);
+  };
+
+
+
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-white shadow-sm border-b border-gray-200">
       <div className="flex items-center space-x-4">
@@ -18,7 +37,12 @@ const Header = () => {
           className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
       </div>
+
+      <Button  onClick={toggleStatus}>
+        {status? 'logout' : 'login'}
+      </Button>
     </header>
+    
   );
 };
 
